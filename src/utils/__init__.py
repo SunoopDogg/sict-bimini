6e9fd452a2a8739d36c9d12d107554924b8c4fe9
SunoopDogg <aswoo55555@gmail.com>
2026-02-02 11:26:50 +0000

    feat: Add BIM attribute conversion utilities

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,16 +2,25 @@
 Utility modules for BIM RAG system.
 """
 
-from .prompt import load_prompt, PROMPTS_DIR
+from .prompt import load_prompt
 from .parsing import parse_json_response
 from .formatters import format_prediction_result
-from .file_selector import select_json_file, DEFAULT_JSON_DIR
+from .file_selector import select_json_file
+from .bim_attribute import BIMAttribute, BIM_ATTRIBUTE_FIELDS
+from .bim_converter import (
+    extract_bim_attribute_from_json,
+    bim_attribute_from_csv_row,
+    format_bim_object_for_prediction,
+)
 
 __all__ = [
     "load_prompt",
-    "PROMPTS_DIR",
     "parse_json_response",
     "format_prediction_result",
     "select_json_file",
-    "DEFAULT_JSON_DIR",
+    "BIMAttribute",
+    "BIM_ATTRIBUTE_FIELDS",
+    "extract_bim_attribute_from_json",
+    "bim_attribute_from_csv_row",
+    "format_bim_object_for_prediction",
 ]
