ad7f5d2c740ccd9d8c43fd05c55ada9d616ea9d8
SunoopDogg <aswoo55555@gmail.com>
2026-02-04 11:27:11 +0000

    chore: Remove unused remove_kbims_property script

--- src/converters/__init__.py
+++ src/converters/__init__.py
@@ -5,7 +5,6 @@ BIM Data Converters Package
 Provides utilities for converting BIM object data between formats.
 """
 
-from .json_to_csv import BIMAttributeExtractor, convert_json_to_csv
 from .xlsx2json import convert_bim_xlsx_from_bytes
 
-__all__ = ['BIMAttributeExtractor', 'convert_json_to_csv', 'convert_bim_xlsx_from_bytes']
+__all__ = ['convert_bim_xlsx_from_bytes']
